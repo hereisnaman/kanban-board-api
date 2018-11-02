6ab832f1a3edb3dea247c7bba550d50af0283722
Naman Kumar <naman@outlook.in>
2018-11-02 11:15:51 +0530

    added signup

--- app/controllers/signup.js
+++ app/controllers/signup.js
@@ -1,6 +1,37 @@
-const signup = (req, res) => {
-  const { name, email, uid } = req.body;
-  console.log(name, email, uid);
+import auth from '../auth';
+import { requestHasura } from '../utils/';
+
+const signup = async (req, res) => {
+  const { name, email, uid, avatar } = req.body;
+
+  try {
+    const { data } = await requestHasura({
+      query: `mutation($user: users_insert_input!) {
+        insert_users(objects: [$user]) {
+          returning {
+            uid
+            email
+            name
+            avatar
+          }
+        }
+      }
+    `,
+      variables: {
+        user: {
+          uid,
+          email,
+          name,
+          avatar,
+        },
+      },
+    });
+
+    return res.send(data.insert_users.returning[0]);
+  } catch (err) {
+    console.log(err);
+    return res.sendStatus(500);
+  }
 };
 
 export default signup;
